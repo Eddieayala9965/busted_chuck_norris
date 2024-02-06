0272324c9833a1f245a20d3a84e16b3b3d8a8280
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-02-06 11:07:54 -0500

    works

--- scripts/get.js
+++ scripts/get.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-function get(url) {
-    return fetch(url)
-        .then(function (response) {
-            return response
-        })
-        .then(function (data) {
-            console.log(data)
-        })
-        .catch(function (error) {
-            console.error('ERROR:', error);
-        });
-}
+const get = (url) => {
+  return fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (error) {
+      console.error("ERROR:", error);
+    });
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,52 @@
-'use strict';
-const categoryListFormEl = document.querySelector('#categoryListForm');
+"use strict";
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM IS READY');
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM IS READY");
+  const categoryListFormEl = document.querySelector("#categoryListForm");
+  const chuckQuote = document.querySelector("#chuckQuote");
 
-    const apiurl = 'https://api.chucknorris.io/jokes/random?category=dev';
+  const apiUrl = "https://api.chucknorris.io/jokes/random?category=dev";
+  generateQuote(apiUrl);
+
+  const categoriesUrl = "https://api.chucknorris.io/jokes/categories";
+  get(categoriesUrl).then(function (response) {
+    console.log("CATEGORY RESPONSE:", response);
+    generateCategoryList(response);
+  });
+
+  categoryListFormEl.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newCategory = this.querySelector("select").value;
+    const apiUrl = `https://api.chucknorris.io/jokes/random?category=${newCategory}`;
+
     generateQuote(apiUrl);
+  });
 
-    const categoriesUrl = 'https://api.chucknorris.io/jokes/categories';
-    get(categoriesUrl).then(function (response) {
-        console.log("CATEGORY RESPONSE:", response);
-        generateCategoryList(response);
-    });
-
-    categoryListFormEl.addEventListener('submit', function (event) {
-        const newCategory = this.querySelector('select').value;
-        const apiUrl = `https://api.chucknorris.io/jokes/random?category=${newCategory}`;
-        generateQuote(apiUrl);
-    })
-});
-
-function generateQuote() {
-
-
+  function generateQuote() {
+    const chuckQuote2 = document.querySelector("#chuckQuote");
     get(apiUrl).then(function (response) {
-        chuckQuote.innerText = response.value
+      chuckQuote2.innerText = response.value;
     });
-}
+  }
 
-
-
-function generateCategoryList(categoryArray) {
-    const selectEl = document.createElement('select');
+  function generateCategoryList(categoryArray) {
+    const selectEl = document.createElement("select");
     // Filter out the explicit category
     const filteredCategories = categoryArray.filter(function (category) {
-        if (category !== 'explicit') {
-            return category;
-        }
+      if (category !== "explicit") {
+        return category;
+      }
     });
-    filterdCategories.map(function (category) {
-        // create an option element
-        const option = document.createElement('option');
-        // define option attributes
-        option.value = category;
-        option.text = category;
-        // append the option to the <select>
-        selectEl.appendChild(option);
+    filteredCategories.map(function (category) {
+      // create an option element
+      const option = document.createElement("option");
+      // define option attributes
+      option.value = category;
+      option.text = category;
+      // append the option to the <select>
+      selectEl.appendChild(option);
     });
     // append the <select> to the <form>
     categoryListFormEl.append(selectEl);
+  }
+});
